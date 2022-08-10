598dc9ab71a394fb95bf6459c57d8b7f9ae55d05
amy chan <amyc125@gmail.com>
2022-08-10 20:07:30 +0100

    add c agent

---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        //Establish inital connection to kafka cluster
+        private const string BootstrapServer = "127.0.0.1:9092";
+        private const string TopicName = "events";
+
+        private static async Task Main()
+        {
+            var producer = new Producer(BootstrapServer);
+            await producer.StartSendingMessages(TopicName);
+        }
+    }
+}
